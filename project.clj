b8f149a3c71d0ccbb9cc1fb7e9f483423442da08
Alvaro Videla <videlalvaro@gmail.com>
2012-09-18 17:54:10 +0200

    changes link to license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject claude "0.1.0-SNAPSHOT"
   :description "Utilities to connect Clojure apps to Cloud Foundry"
   :url "http://example.com/FIXME"
-  :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/mit-license.php"}
+  :license {:name "Apache 2.0 License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :profiles {:dev {:resource-paths ["test/resources"]}}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.3"]]
